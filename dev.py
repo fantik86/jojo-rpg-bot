a5de59aeaabe9e7778e6f63b23498a5284d21a49
ViZus <86660567+ViZus-s@users.noreply.github.com>
2021-11-01 20:03:46 +0300

    Проверка тест команды

--- dev.py
+++ dev.py
@@ -12,7 +12,18 @@ class Devs(commands.Cog):
         self.bot = bot
     @commands.command()
     async def test_cmd1(self,ctx):
-        ...
+        if ctx.author.id in developers:
+            try:
+                channel = self.bot.get_channel(903703988225052762)
+                await channel.send(embed=disnake.Embed(title='Вызвана команда: "stand_inv"', description=f"`ID Автора`: {ctx.author.id}\n`Ник Автора`: {ctx.author}\n`ID Сервера`: {ctx.guild.id}\n`Название Сервера`: {ctx.guild}"))
+            except Exception:
+                pass
+            if collection_name_UserData.count_documents({"_id": f"{ctx.author.id}"}) != 0:
+                arrows_len = collection_name_UserData.find_one({"_id": f"{ctx.author.id}"})["arrows"]
+                money_len = collection_name_UserData.find_one({"_id": f"{ctx.author.id}"})["money"]
+                await ctx.send(embed=disnake.Embed(title="Инвентарь", description=f"Количество стрел:{arrows_len}\nБаланс: {money_len}$"))
+            else:
+                await ctx.send("У вас нет стенда, используйте команду `get_stand` чтобы получить свой первый стенд!")
     @commands.command()
     async def test_cmd2(self, ctx, arg=1):
         ...
