68b3eed8e5bfae459e0056570b40eb05e56be8d6
ViZus <86660567+ViZus-s@users.noreply.github.com>
2021-12-15 15:53:02 +0300

    Сокращение кода и удаление не нужного.

--- starting.py
+++ starting.py
@@ -8,12 +8,8 @@ class Starterss(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('------')
-        print('Произошло логирование в:')
         print(f"Ник и тэг = {self.bot.user}")
         print(f"Мой айди = {self.bot.user.id}")
-        print('------')
-        await self.bot.change_presence(status=disnake.Status.online, activity=disnake.Game("60%"))
 # -------------------------------
 def setup(bot):
     bot.add_cog(Starterss(bot))
