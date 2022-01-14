a879c73f09c3884d81d6881ed7e3bb2252de59da
ViZus <86660567+ViZus-s@users.noreply.github.com>
2022-01-14 20:33:49 +0300

    Была найдена ошибка с кд

--- main.py
+++ main.py
@@ -35,35 +35,6 @@ db = client['DatabaseJoJoRPGBot']
 collection_name_UserData = db['UserData']
 collection_name_TestData = db['TestData']
 channel_id_logs = 903703988225052762
-# ------------------------------- 
-@bot.event
-async def on_message(message):
-    if message.content == f"<@!{896108733275439165}>":
-        await message.channel.send(embed=disnake.Embed(title="Бонджорно!", description="Я тут, если что, вызывай меня через *$*.", color=0xFFFF00))
-    await bot.process_commands(message)
-@bot.event
-async def on_guild_join(guild):
-    try:
-        channel = bot.get_channel(903701604014891040)
-        joinguild = bot.get_guild(guild)
-        await channel.send(embed=disnake.Embed(title='Бот был приглашён на сервер', description=f"`ID Сервера`: {guild}\n`Ник Автора`: {joinguild}`Название Сервера`: {guild}"))
-    except Exception as rrr:
-        t = bot.get_channel(896109675873984543)
-        await t.send(e)
-    for chan in guild.text_channels:
-        if chan.permissions_for(guild.me).send_messages:
-            await chan.send(embed=disnake.Embed(title="Я Джорно Джованна, и у меня есть мечта!", description="Бон-джорно! Меня зовут Джорно Джованна, и у меня есть мечта.\nСпасибо что вы пригласили меня на сервер, я постараюсь представить себя во всей красе!\nЯ являюсь RPG ботом, где вы сможете получить свой стенд, развиваться и бороться с другими.\n\nКоманды - `$help`\nМой префикс - **$**", color=0xffff00))
-            break
-@bot.event
-async def on_guild_leave(guild):
-    try:
-        channel = bot.get_channel(903701604014891040)
-        joinguild = bot.get_guild(guild)
-        await channel.send(embed=disnake.Embed(title='Бот был удалён с сервера', description=f"`ID Сервера`: {joinguild.id}\n`Ник Автора`: {joinguild.owner.id}\n`Количество юзеров`: {joinguild.member.count}\n`Название Сервера`: {guild}"))
-    except Exception as rrrr:
-        print("Send error:", rrrr)
-    return
-    # тут будет форма удаления из бд
 # -------------------------------
 for cog in cooogs:
     bot.load_extension(cog)
